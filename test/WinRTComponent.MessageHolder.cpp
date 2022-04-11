4197112b82f8fb6f7355d04acdc01d342db3e62d
Park DongHa <luncliff@gmail.com>
2022-04-11 23:49:40 +0900

    Build with MIDL3 sources
    * CMake scripts from https://github.com/microsoft/onnxruntime/releases/tag/v1.11.0
    * sample MessageHolder.idl
    * include MIDL3 generated/edited sources, definition files
    * PreCompiled Header for the generated sources

---
+++ test/WinRTComponent.MessageHolder.cpp
@@ -0,0 +1,12 @@
+﻿#include "WinRTComponent.MessageHolder.h"
+#include "module.g.cpp"
+
+namespace winrt::WinRTComponent::implementation {
+hstring MessageHolder::Message() {
+    return m_message;
+}
+
+void MessageHolder::Message(hstring value) {
+    m_message = std::move(value);
+}
+} // namespace winrt::WinRTComponent::implementation
